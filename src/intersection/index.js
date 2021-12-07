e65a99c35df49b62e2c920db6f5b0b6e16b3fc63
Collin Clifford <collinclifford@ymail.com>
2021-12-06 21:20:50 -0800

    sameLetters

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -12,23 +12,6 @@ convert the Set to an array and return
 */
 
 function intersection(a, b) {
-  let aMap = new Map();
-  let aSet = new Set();
-  for (let e = 0; e < a.length; e++) {
-    aMap.set(a[e]);
-  }
-  for (let e = 0; e < b.length; e++) {
-    if (aMap.has(b[e])) {
-      aSet.add(b[e]);
-    }
-  }
-  let aArray = [...aSet];
-  return aArray;
-}
-
-/*
-BOOK SOLUTION
-function intersection(a, b) {
   const map = new Map();
   const results = new Set();
   for (let e of a) {
@@ -43,6 +26,5 @@ function intersection(a, b) {
 
   return Array.from(results);
 }
-*/
 
 module.exports = intersection;
